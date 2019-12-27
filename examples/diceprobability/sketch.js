7b18d4043f1690ff97f909b90efd7cc1a0409e80
temchik76 <76@temchik.com>
2019-12-27 12:39:08 -0500

    dice probability example

---
+++ examples/diceprobability/sketch.js
@@ -0,0 +1,67 @@
+let grid;
+let headerRenderer;
+let renderer;
+
+let values = [];
+let probs = [];
+let totalValues = 0;
+
+class Header extends GridHeaderRenderer {
+  draw(pos, bounds) {
+    //super.draw(pos, bounds);
+    stroke(255);
+    noFill();
+    textAlign(CENTER, CENTER);
+    textSize(18);
+    text(pos + 1, bounds.x + bounds.w / 2, bounds.y + bounds.h / 2);
+  }
+}
+
+class Cell extends GridCellRenderer {
+  draw(col, row, bounds) {
+    super.draw(col, row, bounds);
+
+    let prob = probs[col + row];
+    let c = map(prob, 0, Math.max(...probs), 0, 255);
+    noStroke();
+    fill(c);
+    rect(bounds.x, bounds.y, bounds.w, bounds.h);
+    
+    textAlign(CENTER, CENTER);
+    textSize(18);
+
+    if (c > 128) {
+      stroke(0);
+      fill(0);
+    } else {
+      stroke(255);
+      fill(255);
+    }
+
+    text(col + row + 2, bounds.x + bounds.w / 2, bounds.y + bounds.h / 2);
+  }
+}
+
+function setup() {
+  createCanvas(420, 420);
+  headerRenderer = new Header();
+  renderer = new Cell();
+
+  grid = new Grid(6, 6, 1, 1, new Bounds(0, 0, width, height),
+                 renderer, headerRenderer, headerRenderer);
+
+  values = new Array(12).fill(0);
+  probs = new Array(12).fill(0);
+}
+
+function draw() {
+  let oneCube = floor(random(0, 6));
+  let twoCube = floor(random(0, 6));
+
+  values[oneCube + twoCube]++;
+  totalValues++;
+  probs[oneCube+twoCube] = Math.floor((values[oneCube + twoCube]) * 100 / totalValues);
+
+  background(0);
+  grid.draw();
+}
